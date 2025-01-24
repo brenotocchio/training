f1e2e28c4aeaa80725a566c99a8bedda7689b963
brenotocchio <breno2007tocchio@gmail.com>
2025-01-24 16:19:37 -0300

    Create calculadorasimples.py

---
+++ python/calculadorasimples.py
@@ -0,0 +1,21 @@
+operacao = input('Digite qual operação deseja fazer [+] | [-] | [*] | [/] : ')
+
+if operacao not in ['+', '-', '*', '/']:
+    print('Nenhuma operação foi selecionada, tente novamente.')
+else:
+    numero_1 = int(input('Digite o primeiro número: '))
+    numero_2 = int(input('Digite o segundo número: '))
+
+    if operacao == '+':
+        print(f'A soma resulta em: {numero_1 + numero_2}')
+
+    elif operacao == '-':
+        print(f'A subtração resulta em: {numero_1 - numero_2}')
+    elif operacao == '*':
+        print(f'A multiplicação resulta em: {numero_1 * numero_2}')
+    elif operacao == '/':
+        resultado = numero_1 / numero_2
+        if resultado.is_integer():
+            print(f'A divisão resulta em: {int(resultado)}')  # Converte para inteiro
+        else:
+            print(f'A divisão resulta em: {resultado}')  # Mantém como float
