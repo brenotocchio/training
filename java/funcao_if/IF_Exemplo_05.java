253f7b831a59119bfdcba1cceeb3a069a649fbab
brenotocchio <breno2007tocchio@gmail.com>
2026-03-13 09:10:25 -0300

    .

---
+++ java/funcao_if/IF_Exemplo_05.java
@@ -0,0 +1,15 @@
+package funcao_if;
+
+import java.util.Scanner;
+
+public class IF_Exemplo_05 {
+    public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
+
+        
+
+
+
+    }
+}
