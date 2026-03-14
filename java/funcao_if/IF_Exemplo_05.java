a260f9961ca25b1caaf62cd244c340afe5061f38
brenotocchio <breno2007tocchio@gmail.com>
2026-03-14 09:49:07 -0300

    Exercícios com IF

--- java/funcao_if/IF_Exemplo_05.java
+++ java/funcao_if/IF_Exemplo_05.java
@@ -7,9 +7,21 @@ public class IF_Exemplo_05 {
         
         Scanner entrada = new Scanner(System.in);
 
-        
+        System.out.println("Digite sua idade:");
+        int idade = entrada.nextInt();
+        entrada.nextLine();
 
+        System.out.println("Você é estudante? (sim/não)");
+        String estudante = entrada.nextLine().toLowerCase();
 
+        if (idade < 18 || estudante.equals("sim")){
+            System.out.println("Você tem direito à meia entrada no cinema.");
+        }
+        else {
+            System.out.println("Você não tem direito à meia entrada no cinema.");
+        }
+
+        entrada.close();
 
     }
 }
---
+++ java/funcao_if/IF_Exemplo_06.java
@@ -0,0 +1,17 @@
+package funcao_if;
+
+public class IF_Exemplo_06 {
+
+    public static void main(String[] args) {
+    
+        boolean temCarteira = false;
+
+        if (!temCarteira) {
+
+            System.out.println("Você não tem carteira de motorista.");
+
+        }
+
+    }
+
+}
