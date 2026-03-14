e76fb2a7802170afdca1a6584f4cfa39b43ad562
brenotocchio <breno2007tocchio@gmail.com>
2026-03-14 10:40:45 -0300

    .

---
+++ java/funcao_if/IF_Exercicio_05.java
@@ -0,0 +1,25 @@
+package funcao_if;
+
+import java.util.Scanner;
+
+public class IF_Exercicio_05 {
+    
+    public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Digite um número: ");
+        int numero = entrada.nextInt();
+
+        if (numero % 3 == 0 && numero % 3 == 0){
+            System.out.println("O número digitado é múltiplo de 3 e 5 ao mesmo tempo.");
+        }
+
+        else{
+            System.out.println("número digitado não é múltiplo de 3 e 5 ao mesmo tempo.");
+        }
+
+
+
+    }
+}
